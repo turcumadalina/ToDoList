aac903a951d52e8d36bb84895c7ece8d60ee4579
Madalina Turcu <c_mturcu@groupon.com>
2018-02-07 16:26:20 +0200

    Updated tests

--- app/src/androidTest/java/com/minimalToDo/tests/Demo.java
+++ app/src/androidTest/java/com/minimalToDo/tests/Demo.java
@@ -1,18 +1,25 @@
 package com.minimalToDo.tests;
 
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.filters.MediumTest;
+import android.widget.ImageView;
 import com.example.avjindersinghsekhon.minimaltodo.R;
 import com.minimalToDo.constants.Timeouts;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
 import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by madalina.turcu on 2/7/18.
@@ -21,23 +28,28 @@ import static org.hamcrest.Matchers.allOf;
 public class Demo extends EspressoTestBase {
 
     @Test(timeout = Timeouts.TEST_TIMEOUT_SHORT)
-    public void fistTest2() throws Exception {
-        onView(allOf(withText("Minimagfgl"), isCompletelyDisplayed())).check(matches(isDisplayed()));
-    }
+    public void testDeleteItem() throws Exception {
+        onView(allOf(withText("Item"), isCompletelyDisplayed())).check(matches(isDisplayed()));
 
-    @Test(timeout = Timeouts.TEST_TIMEOUT_SHORT)
-    public void fistTest4() throws Exception {
-        onView(allOf(withText("Tujtur"), isCompletelyDisplayed())).check(matches(isDisplayed()));
+        onView(allOf(withText("Item"), isCompletelyDisplayed())).perform(swipeLeft());
     }
 
     @Test(timeout = Timeouts.TEST_TIMEOUT_MEDIUM)
-    public void fistTest() throws Exception {
+    public void testAppNameClickItemAndCheckRemindMe() throws Exception {
         onView(allOf(withText(R.string.app_name), isCompletelyDisplayed())).check(matches(isDisplayed()));
 
-        onView(allOf(withText("Tujtur"), isCompletelyDisplayed())).perform(click());
+        onView(allOf(withText("Test"), isCompletelyDisplayed())).perform(click());
 
         Espresso.pressBack();
 
         onView(allOf(withText(R.string.remind_me), isCompletelyDisplayed())).check(matches(isDisplayed()));
+    }
+
+    @Test(timeout = Timeouts.TEST_TIMEOUT_SHORT)
+    public void testMenuButton() throws Exception {
+        ViewInteraction menu = onView(allOf(is(instanceOf(ImageView.class)), isCompletelyDisplayed(), withParent(hasSibling(withText(R.string.app_name)))));
+
+        menu.check(matches(isDisplayed()));
+        menu.perform(click());
     }
 }
